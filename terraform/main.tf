1a4f6ce7a19622a41a52db506051b98190469ed7
jonascicek <jonascicek98@gmail.com>
2025-06-18 15:54:04 +0200

    Test push 9

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,7 +58,7 @@ resource "aws_security_group" "web" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-0fc5d935ebf8bc3bc" # Ubuntu 22.04 in eu-central-1
+  ami           = ami = "ami-06dd92ecc74fdfb36" # Ubuntu 22.04 in eu-central-1
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.web.id]
